2cb790e2a46e085b5d168132b059217c0f5000c6
Richard OReilly <richardjosephoreilly.com>
2013-03-15 00:13:32 -0400

    first push. fart

---
+++ app/assets/javascripts/appjazz.js
@@ -0,0 +1,5 @@
+ $(function(){
+              //hide the fadein element with js and then fade it back in
+              $('[data-behavior~=fadein]').hide();
+              $('[data-behavior~=fadein]').delay(1000).fadeIn(2000);
+            });//end  function
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Except::Application.config.secret_token = '1456f04914f786c1ef358c3733986e6ad090f430f2d10a9305f82942b6422e0819b3ecb56aec0f0f33e87a0111a0550eb3cb499ad9f6da3dcd48aceba4b07317'
