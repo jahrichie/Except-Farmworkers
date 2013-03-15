58ffd87d05548a85b2c91ad1a46507a8b9e22b59
Richard OReilly <richardjosephoreilly.com>
2013-03-15 01:35:47 -0400

    totall busted

--- app/assets/javascripts/appjazz.js
+++ app/assets/javascripts/appjazz.js
@@ -1,4 +1,10 @@
  $(function(){
+
+              $('#navbar').scrollspy();
+              $('[data-spy="scroll"]').each(function () {
+                var $spy = $(this).scrollspy('refresh')
+              });
+
               //hide the fadein element with js and then fade it back in
               $('[data-behavior~=fadein]').hide();
               $('[data-behavior~=fadein]').delay(1000).fadeIn(2000);
